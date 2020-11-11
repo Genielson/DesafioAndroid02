e12012c319dcb2f3a96dd1a45dfe76e540fdb4af
Genielson <ge.nielson@hotmail.com>
2020-11-11 17:55:34 -0300

    Adicionada validacao dos campos de Login

--- app/src/main/java/com/example/desafioandroid02/CadastrarUserActivity.kt
+++ app/src/main/java/com/example/desafioandroid02/CadastrarUserActivity.kt
@@ -2,10 +2,51 @@ package com.example.desafioandroid02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class CadastrarUserActivity : AppCompatActivity() {
+
+    private lateinit var buttonRegisterUser:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastrar_user)
+
+        buttonRegisterUser = findViewById(R.id.btnSignup)
+
+        buttonRegisterUser.setOnClickListener {
+
+
+
+        }
+
     }
+
+    fun validaCamposCadastro():Boolean{
+
+        var camposForamValidados = true
+
+        if(textFieldEmailContent.text!!.trim()!!.isBlank()){
+
+            textFieldEmailContent.error = getString(R.string.empty_email)
+
+            camposForamValidados = false
+
+        }
+
+        if(textFieldPasswordContent.text!!.trim().isBlank()){
+
+            textFieldPasswordContent.error = getString(R.string.empty_password)
+
+            camposForamValidados = false
+
+        }
+
+        return camposForamValidados
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/desafioandroid02/MainActivity.kt
+++ app/src/main/java/com/example/desafioandroid02/MainActivity.kt
@@ -1,16 +1,71 @@
 package com.example.desafioandroid02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var buttonRegister:Button
 
+    private lateinit var buttonLogin:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buttonRegister = findViewById<Button>(R.id.btnRegister)
+
+        buttonLogin = findViewById<Button>(R.id.btnLogin)
+
+        buttonLogin.setOnClickListener {
+
+            if(validaCamposLogin()){
+
+            }
+
+        }
+
+        btnRegister.setOnClickListener {
+
+            chamarTelaRegister()
+
+        }
+
     }
+
+    fun validaCamposLogin():Boolean{
+
+        var camposForamValidados = true
+
+        if(textFieldEmailContent.text!!.trim()!!.isBlank()){
+
+            textFieldEmailContent.error = getString(R.string.empty_email)
+
+            camposForamValidados = false
+
+        }
+
+        if(textFieldPasswordContent.text!!.trim().isBlank()){
+
+            textFieldPasswordContent.error = getString(R.string.empty_password)
+
+            camposForamValidados = false
+
+        }
+
+        return camposForamValidados
+
+    }
+
+    fun chamarTelaRegister(){
+
+        var intent = Intent(this,CadastrarUserActivity::class.java)
+
+        startActivity(intent)
+    }
+
 
 }
